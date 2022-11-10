b0c5753febd61df2d902329ac4baf349d4a4d9e0
Twoje Imię Nazwisko <toja@example.com>
2022-11-10 06:30:37 +0000

    commit

---
+++ pull.php
@@ -0,0 +1,17 @@
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+//hellofff
+
+
+if ( $_POST['payload'] ) {
+putenv('PATH=/usr/local/bin');
+echo shell_exec('cd /home/weblide2/www/rust && /usr/bin/git pull origin main 2>&1');
+echo shell_exec('/usr/bin/whoami 2>&1');
+echo 'mateusz2';
+}
+
+
+//robcio
+?>
+
